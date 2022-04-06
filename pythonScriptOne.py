2fed8fdbb3f226348d19e3d4f75be1ffb455c05e
oscarsnva15 <oscarsnva15@gmail.com>
2022-04-05 23:09:45 -0500

    add:Dockfile_first

---
+++ pythonScriptOne.py
@@ -0,0 +1,36 @@
+print("I'm execute the Python code")
+print("so whatch it")
+print("Autor: @Oscar Suarez Nava\n")
+
+#create a variable , to create a 
+color = "blue"
+print(color+"\n");
+
+#here is the table of available aritmethic operations:
+'''
+a + b adition SUM of a and b
+a - b Subtraction Diference of a and b
+a * b Multiplication Product of a and b
+a / b True division Quotient of a and b
+a// b Floor division Quotient od a and b , removing fractional parts
+a % b Modulus Integer remainder after division
+a ** b Exponentation a raised to the power of b
+-a Negation The negative of a
+
+'''
+numberOne=5
+numberTwo=20
+print("------ TEST OF OPERATORS ARITMETICS------")
+res_sum = "the result of "+ str(numberOne) +" + "+ str(numberTwo) +" is " + str(numberOne+numberTwo)
+print(res_sum)
+res_rest = "the result of "+ str(numberOne) +" - "+ str(numberTwo) +" is " + str(numberOne-numberTwo)
+print(res_rest)
+res_mult = "the result of "+ str(numberOne) +" * "+ str(numberTwo) +" is " + str(numberOne*numberTwo)
+print(res_mult)
+res_div = "the result of "+ str(numberOne) +" / "+ str(numberTwo) +" is " + str(numberOne/numberTwo)
+print(res_div)
+res_doublePai = "the result of "+ str(numberOne) +" //"+ str(numberTwo) +" is " + str(numberOne//numberTwo)
+print(res_doublePai)
+res_doubleMult = "the result of "+ str(numberOne) +" **"+ str(numberTwo) +" is " + str(numberOne**numberTwo)
+print(res_doubleMult)
+
